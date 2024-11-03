3ebc77ab89bb101c89c88cf59ba6631426596250
fbc <fbc@fbc.lan>
2024-11-03 17:18:08 +0100

    create models for department, employee and dependent

--- acmevita/user_department_api/models.py
+++ acmevita/user_department_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    """model for departments from ACMEVita"""
+    name = models.CharField(max_length=100, unique=True, db_index=True)
+
+
+class Employee(models.Model):
+    """model for employees from ACMEVita"""
+
+    full_name = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, related_name="employees")
+
+    @property
+    def have_dependents(self):
+        return self.dependents.exists()
+    
+
+class Dependent(models.Model):
+    name = models.CharField(max_length=100)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="dependents")
