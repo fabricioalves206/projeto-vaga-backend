09d5263474364a1695815f21bb84a6e9c8628676
Fabricio <fabricio.alves2698@gmail.com>
2024-11-03 15:28:03 -0300

    create requested views with drf

--- acmevita/user_department_api/views.py
+++ acmevita/user_department_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Department, Employee
+from .serializers import DepartmentSerializer, EmployeeSerializer
 
-# Create your views here.
+class DepartmentListView(generics.ListAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class EmployeeListView(generics.ListAPIView):
+    serializer_class = EmployeeSerializer
+
+    def get_queryset(self):
+        department_id = self.kwargs['department_id']
+        return Employee.objects.filter(department__id=department_id)
