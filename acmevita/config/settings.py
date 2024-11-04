0e2e17a687aa28df04bc8705415884bdc6d30e5a
Fabricio <fabricio.alves2698@gmail.com>
2024-11-03 21:01:58 -0300

    fix get environment variable DEBUG

--- acmevita/config/settings.py
+++ acmevita/config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
